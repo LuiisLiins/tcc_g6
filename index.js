1fea1746ba196639b1eaeb87c7e8cc1228427ed0
LuiisLiins <fgsoato2006@gmail.com>
2023-09-01 10:59:04 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express'); 
+
+const app = express(); 
+
+// const porta = process.env.PORT || 3333;
+const porta = 3333;
+
+app.listen(porta, () => {
+    console.log('Servidor iniciado na porta ' + porta); 
+    //console.log(`Servidor iniciado na porta ${porta}`);
+});
+
+app.get('/', (request, response) => {
+    response.send('Hello World');
+});
