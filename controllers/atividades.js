272db27b9a6d05a118cd74fd99bce78a9b8c1889
DanielebBorges <143644899+DanielebBorges@users.noreply.github.com>
2023-10-25 15:25:20 -0300

    25-10

--- controllers/atividades.js
+++ controllers/atividades.js
@@ -7,15 +7,31 @@ module.exports = {
             const sql = 'SELECT atv_id, atv_dthr, atv_status, prod_id, usu_id, set_id FROM ATIVIDADES;';
             const atividades = await db.query(sql);
             const nReg = atividades[0].lenght;
-            return response.status(200).json({confirma: 'Sucesso', 'nItens': nReg, 'itens': atividades[0]});
-        } catch (error) {
+            return response.status(200).json(
+                {confirma: 'Sucesso',
+                message: 'Atividades cadastradas',
+                nItens: nReg, 
+                itens: atividades[0]
+            }
+        );
+   } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
         }
     }, 
                   
     async cadastrarAtividades(request, response) {
         try {
-            return response.status(200).json({confirma: 'cadastrar atividades'});
+            const {atv_dthr, atv_status, prod_id, usu_id, set_id} = request.body;
+            const sql = 'INSERT INTO atividades (atv_dthr, atv_status, prod_id, usu_id, set_id) VALUES (?, ?, ?, ?, ?, ?) ;';
+            const values = [atv_id, atv_dthr, atv_status, prod_id, usu_id, set_id];
+            const confirmacao = await db.query(sql,values);
+            const atv_id = confirmacao[0].insertId;
+
+            return response.status(200).json(
+                {confirma: 'Sucesso',
+                 message:  'cadastro de atividades concluido',
+                 atv_id
+                });
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
         }
